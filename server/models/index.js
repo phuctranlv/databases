292c9f6d03954355c113c871fc6f092165fd130b
phuctranlv <phuctranlv@gmail.com>
2020-02-29 10:06:29 -0800

    fully implement message and user post queries. passes the first test!

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,11 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () { }, // a function which produces all the messages
+    get: function () {
+      console.log('about to run get message query');
+      // var query = 'SELECT * from '
+
+    }, // a function which produces all the messages
     post: function (requestBody, cb) {
       //(later, run a SELECT query to check for existing users with the same name
 
@@ -16,7 +20,7 @@ module.exports = {
         if (error) {
           console.log('Error in model:', error);
         }
-        console.log('Results in model:', results);
+        // console.log('Results in model:', results);
         cb(error, results);
       });
       //call callback based on results of request
@@ -31,9 +35,10 @@ module.exports = {
 
       //send mysql INSERT request with username
       console.log('about to run users db.query');
-      var query = `INSERT INTO users (userName) VALUES ('${username}');`;
-      console.log(query);
-      db.query(query, (error, results, fields) => {
+      var queryParams = [`${username}`];
+      var query = 'INSERT INTO users (userName) VALUES (?);';
+      // console.log(query);
+      db.query(query, queryParams, (error, results, fields) => {
         if (error) {
           console.log('Error in model:', error);
         }
